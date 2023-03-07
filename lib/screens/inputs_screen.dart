3c88c4791ec104c48b6f97942235293402bcd392
Luis Rivas <luisricardorivasgiwencer@gmail.com>
2023-03-07 18:58:49 -0400

    drop down menu added

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -65,6 +65,23 @@ class InputScreen extends StatelessWidget {
               const SizedBox(
                 height: 30,
               ),
+              DropdownButtonFormField<String>(
+                  value: 'Admin',
+                  items: const [
+                    DropdownMenuItem(value: 'Admin', child: Text('Admin')),
+                    DropdownMenuItem(
+                        value: 'Developer', child: Text('Developer')),
+                    DropdownMenuItem(
+                        value: 'Superuser', child: Text('Superuser')),
+                    DropdownMenuItem(
+                        value: 'Jr. Developer', child: Text('Jr. Developer')),
+                  ],
+                  onChanged: (value) {
+                    formValues['role'] = value ?? 'Admin';
+                  }),
+              const SizedBox(
+                height: 30,
+              ),
               ElevatedButton(
                 onPressed: () {
                   FocusScope.of(context).requestFocus(FocusNode());
